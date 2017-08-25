488de5d75d58391c98936ef3c59d9dbdf0052a9d
Benoit BRIATTE <briatte.benoit@digipolitan.com>
2017-08-25 22:55:40 +0200

    update Carthage & SPM

--- Package.swift
+++ Package.swift
@@ -1,7 +1,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "AlamofireLogging",
-    dependencies: [
-    ]
+  name: "AlamofireLogging",
+  dependencies: [
+    .Package(url: "https://github.com/Alamofire/Alamofire.git", versions: Version(4, 0, 0)..<Version(5, 0, 0))
+  ],
+  exclude: [
+    "Tests",
+    "Samples"
+  ]
 )
